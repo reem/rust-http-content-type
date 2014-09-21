70a768ab1367e326b72cf84a2f23b99b5839e947
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-09-21 18:07:37 +0200

    Make this compile in Windows
    Replace `generate` by a python script
    Use the `regex` crate within Rust to replace awk

--- src/generator/mimegen.rs
+++ src/generator/mimegen.rs
@@ -1,34 +1,20 @@
 use std::io::IoResult;
 use std::collections::hashmap::HashMap;
-use std::str::from_utf8;
 
 use super::{get_file_reader, get_file_writer};
-
-macro_rules! parse_word(
-    ($iter:ident, $word:ident, $breaker:pat, $next:expr) => (
-        // Loop to parse a word out of a line
-        match $iter.next() {
-            Some(Ok($breaker)) => break,
-            Some(Ok(c)) => $word.push(c),
-            Some(Err(e)) => return Err(e),
-            None => $next
-        }
-    );
-)
 
 // Generate response/mimes/mod.rs
 pub fn generate(list: Path, module: Path) -> IoResult<()> {
     let mut reader = get_file_reader(list);
     let mut writer = get_file_writer(module);
 
-    try!(writer.write(
-b"// This is an automatically generated file.
+    try!(writer.write(b"\
+// This is an automatically generated file.
 
 use http::headers::content_type::MediaType;
 
 pub fn get_generated_content_type(ext: &str) -> Option<MediaType> {
-    match ext {
-"   ));
+    match ext {"));
 
     /* Generated snippets will look like:
     "json" => Some(MediaType {
@@ -38,32 +24,28 @@ pub fn get_generated_content_type(ext: &str) -> Option<MediaType> {
     }),
     */
 
-    let mut byter = reader.bytes();
-    // avoid duplicates
-    let mut seen = HashMap::new();
-    'read: loop {
-        let mut ext = vec![];
-        let mut type_ = vec![];
-        let mut subtype = vec![];
-        
-        loop { parse_word!(byter, ext, b' ', break 'read); }
-        loop { parse_word!(byter, type_, b' ', break 'read); }
-        loop{ parse_word!(byter, subtype, b'\n', break 'read); }
+    let re = regex!(r"(\S*)/(\S*)\s*(\S*)");
+    let mimes = reader.read_to_string().unwrap();
+    let mut seen = HashMap::new(); // Avoid duplicates
+    for line in mimes.as_slice().lines() {
+        // Ignore commented lines
+        if line.char_at(0) == '#' { continue }
 
+        // Everything should match
+        let caps = re.captures(line).unwrap();
+        let (ext, type_, subtype) = (caps.at(3), caps.at(1), caps.at(2));
         if !seen.contains_key(&ext) {
-
-            try!(write!(writer,
-"    \"{}\" => Some(MediaType {{
-        type_: \"{}\".to_string(),
-        subtype: \"{}\".to_string(),
-        parameters: vec![]
-    }}),\n", from_utf8(ext.as_slice()).unwrap(),
-             from_utf8(type_.as_slice()).unwrap(),
-             from_utf8(subtype.as_slice()).unwrap()));
+            try!(write!(writer, "
+        \"{}\" => Some(MediaType {{
+            type_: \"{}\".to_string(),
+            subtype: \"{}\".to_string(),
+            parameters: vec![]
+        }}),", ext, type_, subtype));
 
             seen.insert(ext, true);
         }
     }
 
-    writer.write(b"        _ => None\n    }\n}\n")
+    writer.write(b"
+        _ => None\n    }\n}\n")
 }
