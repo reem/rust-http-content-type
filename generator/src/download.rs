f001f1defc2f7a647d1954685f3e134fd13da5fa
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-09-22 21:31:50 +0200

    Use a compiler plugin to generate the media types
    The code has been split into two crates, one for the generator and other
    for the content-type.
    
    The custom build script is no longer needed, since everything is now done
    via Rust.

---
+++ generator/src/download.rs
@@ -0,0 +1,51 @@
+use http::client::RequestWriter;
+use http::method::Get;
+use std::collections::HashSet;
+use url::Url;
+
+// Return a string containing the text at the given url, or an error message
+pub fn download_mimes(url: &str) -> Result<String, String> {
+    let parsed_url = match Url::parse(url) {
+        Ok(u)  => u,
+        Err(e) => return Err(format!("Unable to parse url: {}", e))
+    };
+    let request: RequestWriter = match RequestWriter::new(Get, parsed_url) {
+        Ok(request) => request,
+        Err(e)      => return Err(format!("Unable to create http request: {}", e.desc)),
+    };
+    let mut response = match request.read_response() {
+        Ok(response) => response,
+        Err((_, e))  => return Err(format!("HTTP request failed: {}", e.desc))
+    };
+    match response.read_to_string() {
+        Ok(s)  => Ok(s),
+        Err(e) => Err(format!("Error while reading response: {}", e.desc))
+    }
+}
+
+// Return a vector of tuples containing the necessary information to generate the MediaTypes.
+// The order is (ext, type_, subtype).
+// If a line does not parse, return an error message.
+pub fn parse_mimes(mimes: &str) -> Result<Vec<(&str, &str, &str)>, String> {
+    let re = regex!(r"(\S*)/(\S*)\s*(\S*)");
+    let mut seen = HashSet::new(); // Avoid duplicates
+    let mut vec = Vec::with_capacity(800);
+
+    for line in mimes.lines() {
+        // Ignore 0-length or commented lines
+        if line.len() == 0 || line.char_at(0) == '#' { continue }
+
+        let caps = match re.captures(line) {
+            Some(captures) => captures,
+            None           => return Err(format!("'{}' does not match the regular expression pattern", line))
+        };
+
+        let (ext, type_, subtype) = (caps.at(3), caps.at(1), caps.at(2));
+        if !seen.contains(&ext) {
+            vec.push((ext, type_, subtype));
+            seen.insert(ext);
+        }
+    }
+
+    Ok(vec)
+}
